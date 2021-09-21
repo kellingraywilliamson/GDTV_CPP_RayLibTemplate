b87cdbde061aed80db2e05f71f2654be2ea715d5
kellingraywilliamson <55054332+kellingraywilliamson@users.noreply.github.com>
2021-09-21 07:11:36 +0100

    First 'project' completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,86 @@
+#include <cstdlib>
+#include <cstdio>
 #include "raylib.h"
 
-constexpr auto SCREEN_WIDTH  = 800;
-constexpr auto SCREEN_HEIGHT = 450;
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
+    int width{800};
+    int height{450};
+
+    int circleX = width / 4;
+    int circleY = height / 2;
+    int circleRadius{25};
+
+    int circleXLeft = circleX - circleRadius;
+    int circleXRight = circleX + circleRadius;
+    int circleYTop = circleY - circleRadius;
+    int circleYBottom = circleY + circleRadius;
+
+    int axaX{400};
+    int axaY{0};
+    int axaWidth{50};
+    int axaHeight{50};
+    int axaDirection{10};
+
+    int axaXLeft = axaX;
+    int axaXRight = axaX + axaWidth;
+    int axaYTop = axaY;
+    int axaYBottom = axaY + axaHeight;
+
+    bool collisionWithAxe{true};
+
+    InitWindow(width, height, "Demo Window");
+
     SetTargetFPS(60);
-
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(WHITE);
 
-        ClearBackground(RAYWHITE);
+        collisionWithAxe = axaYBottom >= circleYTop
+                           && axaYTop <= circleYBottom
+                           && axaXLeft <= circleXRight
+                           && axaXRight >= circleXLeft;
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+        if (collisionWithAxe)
+        {
+            DrawText("Game Over!", 400, 200, 20, RED);
+        }
+        else
+        {
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+            circleXLeft = circleX - circleRadius;
+            circleXRight = circleX + circleRadius;
+            circleYTop = circleY - circleRadius;
+            circleYBottom = circleY + circleRadius;
+
+            axaXLeft = axaX;
+            axaXRight = axaX + axaWidth;
+            axaYTop = axaY;
+            axaYBottom = axaY + axaHeight;
+
+            DrawCircle(circleX, circleY, circleRadius, BLUE);
+            DrawRectangle(axaX, axaY, axaWidth, axaHeight, RED);
+
+            axaY += axaDirection;
+            if (axaY > (height - axaHeight) || axaY < 0)
+            {
+                axaDirection *= -1;
+            }
+
+
+            if (IsKeyDown(KEY_D) && circleX < width)
+            {
+                circleX += 10;
+            }
+            else if (IsKeyDown(KEY_A) && circleX > 0)
+            {
+                circleX -= 10;
+            }
+        }
 
         EndDrawing();
+
     }
-
-    CloseWindow();
-
-    return 0;
 }
